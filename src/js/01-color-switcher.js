1207e2cc47a676a4fc7df1a5e92da5437b45187d
YevheniiGrabina <89870811+YevheniiGrabina@users.noreply.github.com>
2022-04-12 22:08:54 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
 
+let intervalId = null;
+
+refs.start.addEventListener('click', backgroundColorChanger);
+refs.stop.addEventListener('click', stopBackgroundColorChanger);
+
+function backgroundColorChanger() {
+  refs.start.setAttribute('disabled', '');
+  refs.stop.removeAttribute('disabled');
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopBackgroundColorChanger() {
+  refs.start.removeAttribute('disabled');
+  refs.stop.setAttribute('disabled', '');
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
